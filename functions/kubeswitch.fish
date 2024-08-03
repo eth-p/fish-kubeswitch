2eebe44e4343fa8a50876c2887357cf2c15347e5
Ethan P. <eth-p+git@hidden.email>
2024-08-02 18:37:07 -0700

    Remove compatibility shim for older fish versions

--- functions/kubeswitch.fish
+++ functions/kubeswitch.fish
@@ -805,7 +805,7 @@ function __kubeswitch_util_message --description="Prints a message"
 	set -l reset_color_code (__kubeswitch_util_color -- normal)
 
 	# Print the message.
-	printf "%s%s%s\n" "$active_color_code" (printf $__promptfessional_printf_accept_pattern_variable $argv) "$reset_color_code"
+	printf "%s%s%s\n" "$active_color_code" (printf $argv) "$reset_color_code"
 	return 0
 end
 
